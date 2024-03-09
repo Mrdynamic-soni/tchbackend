50a6a91403ae58f1ad4ca3664ae94bef9c480557
Mrdynamic-soni <ssoni5930@gmail.com>
2024-03-09 12:10:26 +0530

    added database connection

---
+++ database.js
@@ -0,0 +1,17 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const client = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '12345',
+    port: 5432,
+});
+
+export const excuteQuery = async (sqlQuery) => {
+    const response = client.query(sqlQuery)
+    if (response)
+        return response;
+    return " Error"
+}
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import express from "express"
+import { excuteQuery } from "./database.js";
 
 const app = express();
 
-const port = 3000;
+const port = 6767;
 
 app.get('/', async(req, res)=>{
-    res.send("Hello I am ljwkgjyrig")
+    res.send("Hello, Server is Up and running")
 })
 
 
-
-app.get('/Home', async(req, res)=>{
-    res.send("Hello I am server")
+app.get("/getdata", async(req, res)=>{
+    const response =await excuteQuery(`select * from users`)
+    console.log(response);
+    res.send(response.rows)
 })
 
-
-
-// Start the server
 app.listen(port, () => {
     console.log(`Server is listening  check at http://localhost:${port}`);
   });
